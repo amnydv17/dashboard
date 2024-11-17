a307649f0add6a24886516f7cfeeba991269bfce
Aman Yadav <yadavaman150902@gmail.com>
2024-11-18 02:12:22 +0530

    Update Code Errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import 'tailwindcss/tailwind.css';
-import type { AppProps } from "next/app";
 import Navbar from '@/pages/components/Navbar'
 import Dashboard from '@/pages/index'
 
-export default function App({}: AppProps) {
+export default function App() {
   return (
     <>
     <Navbar/>
